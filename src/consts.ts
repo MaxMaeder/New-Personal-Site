7df06819808d7f8e93bd1cfd970de6372e35f337
Max Maeder <max.s.maeder@gmail.com>
2025-01-27 16:07:59 -0600

    Add images

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,4 @@
 export const SITE_TITLE = "Max Maeder";
-export const SITE_DESCRIPTION = "";
-export const SITE_SHARE_IMAGE = "";
+export const SITE_DESCRIPTION =
+  "Computer & Data Science student at UW Madison. Currently at Talos Trading and Ramp, previously at Capital One.";
+export const SITE_PREVIEW_IMAGE = "preview-img.png";
