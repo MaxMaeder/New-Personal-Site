ca25a86def7365417be7dd8756555c1a4819103f
Max Maeder <max.s.maeder@gmail.com>
2025-01-29 16:59:02 -0600

    Add capynow stuff

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = "Max Maeder";
 export const SITE_DESCRIPTION =
-  "Computer & Data Science student at UW Madison. Incoming intern at Talos Trading then at Ramp, previously at Capital One.";
+  "Computer & Data Science student at UW Madison. Incoming intern at Talos Trading and at Ramp, previously at Capital One.";
 export const SITE_PREVIEW_IMAGE = "/preview-img.png";
